7775a7ddbb40872fc33451a34454df0b58f275d2
Moisés Madeira <msalcmd@gmail.com>
2022-11-02 10:08:26 -0300

    refatoração e finalização de métodos

--- Gestao/main.py
+++ Gestao/main.py
@@ -2,8 +2,46 @@ import os
 import time
 from sys import platform
 
+""" Variável em contexto global que armazenará os dados dos veículos """
+veiculos = (
+    {
+        "numero": 1,
+        "nome": "Ford Mustang GT",
+        "marca": "Ford",
+        "ano": "2015",
+        "alugado": False,
+        "cor": "branco"
+    },
+    {
+        "numero": 2,
+        "nome": "Hyundai Tiburon GT V6",
+        "marca": "Hyundai",
+        "ano": "2016",
+        "alugado": False,
+        "cor": "cinza",
+    },
+    {
+        "numero": 3,
+        "nome": "Lamborghini Murciélago",
+        "marca": "Lamborghini",
+        "ano": "2017",
+        "alugado": False,
+        "cor": "preto",
+    },
+    {
+        "numero": 4,
+        "nome": "Nissan 350Z",
+        "marca": "Nissan",
+        "ano": "2018",
+        "alugado": False,
+        "cor": "verde perolado",
+    }
+)
 
-def print_personalizado(texto, tipo):
+""" imprime um texto e uma formatação de acordo com argumentos """
+def print_personalizado(texto, formato):
+
+    """ contém um dict com nomes de cores e os respectivos códigos """
     dict_cores = {
         "roxo": "\033[95m",
         "azul": "\033[94m",
@@ -13,40 +51,13 @@ def print_personalizado(texto, tipo):
         "vermelho": "\033[91m",
         "negrito": "\033[1m",
         "sublinhado": "\033[4m",
-        "padrão": "\033[0m",
+        "fonte_padrao": "\033[0m",
     }
-    if tipo in dict_cores:
-        print(dict_cores[tipo] + texto + dict_cores["padrão"])
+
+    if formato in dict_cores:
+        print(dict_cores[formato] + texto + dict_cores["fonte_padrao"])
     else:
         print(texto)
-
-
-veiculos = (
-    {
-        "nome": "Ford Mustang GT",
-        "marca": "Ford",
-        "ano": "2015",
-        "alugado": False,
-    },
-    {
-        "nome": "Hyundai Tiburon GT V6",
-        "marca": "Hyundai",
-        "ano": "2016",
-        "alugado": False,
-    },
-    {
-        "nome": "Lamborghini Murciélago",
-        "marca": "Lamborghini",
-        "ano": "2017",
-        "alugado": False,
-    },
-    {
-        "nome": "Nissan 350Z",
-        "marca": "Nissan",
-        "ano": "2018",
-        "alugado": False,
-    }
-)
 
 
 def limpa_tela():
@@ -57,7 +68,7 @@ def limpa_tela():
 
 
 def aguarda_tecla():
-    print('\nAperte qualquer tecla para continuar.')
+    print_personalizado('\nAperte qualquer tecla para continuar.', 'roxo')
     input()
 
 
@@ -73,11 +84,11 @@ def mostra_tela_inicial():
     ''')
 
 
-def mostra_veiculo(veiculo, numero):
+def mostra_veiculo(veiculo):
     print(
         f'''
 =============================Veículo================================    
-    Nº {numero} - {veiculo["marca"]}, {veiculo["ano"]} - {"Alugado" if veiculo["alugado"] else "Disponível para alugar"} 
+    Nº {veiculo["numero"]} - {veiculo["nome"]}, {veiculo["marca"]}, {veiculo["ano"]} - {"Alugado" if veiculo["alugado"] else "Disponível para alugar"} 
 ====================================================================
 ''')
 
@@ -87,13 +98,13 @@ def ver_veiculos():
     print()
     if opcao == 't':
         for num_veiculo, veiculo in enumerate(veiculos, start=1):
-            mostra_veiculo(veiculo, num_veiculo)
+            mostra_veiculo(veiculo)
             time.sleep(0.4)
         aguarda_tecla()
 
     elif opcao == 'v':
         opcao_procura = int(input('''Você quer procurar por:
-        1) Por número(id)
+        1) Por número
         2) Por marca
         3) Por ano
         4) Alugados
@@ -106,7 +117,12 @@ def ver_veiculos():
         if opcao_procura == 1:
             limpa_tela()
             for veiculo in filter(lambda v: veiculos.index(v) == opcao_valor - 1, veiculos):
-                mostra_veiculo(veiculo, veiculos.index(veiculo) + 1)
+                mostra_veiculo(veiculo)
+            aguarda_tecla()
+        elif opcao_procura == 2:
+            limpa_tela()
+            for veiculo in filter(lambda v: v["marca"] == veiculo["marca"], veiculos):
+                mostra_veiculo(veiculo)
             aguarda_tecla()
 
 
@@ -116,7 +132,7 @@ def adicionar_veiculo():
     print('Adicionar veículo:')
     nome_veiculo = input('Qual é o nome? ')
     marca_veiculo = input('Qual é a marca? ')
-    ano_veiculo = int(input('Qual é o ano?'))
+    ano_veiculo = int(input('Qual é o ano? '))
     veiculo = {
         "nome": nome_veiculo,
         "marca": marca_veiculo,
@@ -126,6 +142,46 @@ def adicionar_veiculo():
     # @todo aqui fazer testes com os valores inseridos...
     veiculos = veiculos + (veiculo,)
 
+
+def editar_veiculo():
+    global veiculos
+    limpa_tela()
+    qtd_veiculos = len(veiculos)
+    numero_inserido = int(input('Qual o número do veículo que deseja editar? '))
+    if qtd_veiculos - 1 > numero_inserido or numero_inserido < 1:
+        print_personalizado(f'Digite um número válido, de 1 a {qtd_veiculos}', 'vermelho')
+    else:
+        veiculo_a_editar = veiculos[numero_inserido - 1]
+        mostra_veiculo(veiculo_a_editar)
+        aspecto_a_editar = int(input("Qual aspecto deseja editar?\n1 - Disponibilidade, 2 - cor, 3 - Desistir "))
+        if aspecto_a_editar == 1:
+            escolha = input(f'o veículo está {"Alugado" if veiculo_a_editar["alugado"] else "Disponível para alugar"}. Deseja mudar o status? s/n ')
+            if escolha == 's':
+                veiculo_a_editar["alugado"] = not veiculo_a_editar["alugado"]
+                mostra_veiculo(veiculo_a_editar)
+                print_personalizado('Modificação realizada.', 'verde')
+                aguarda_tecla()
+        elif aspecto_a_editar == 2:
+            veiculo_a_editar["cor"] = input(f'o veículo está com a cor {veiculo_a_editar["cor"]}. Digite a nova cor: ')
+            mostra_veiculo(veiculo_a_editar)
+            print_personalizado('Modificação realizada.', 'verde')
+            aguarda_tecla()
+        else:
+            print_personalizado('Saindo...', 'amarelo')
+
+def remover_veiculo():
+    global veiculos
+    veiculos_lista_temp = list(veiculos)
+    limpa_tela()
+    numero_veiculo_a_remover = int(input("Qual o número do veículo que deseja remover? "))
+    for veiculo in veiculos:
+        if veiculo["numero"] == numero_veiculo_a_remover:
+            mostra_veiculo(veiculo)
+            confirmacao = input("Este é o veículo que deseja remover? s/n ")
+            if confirmacao == 's':
+                veiculos_lista_temp.remove(veiculo)
+                print_personalizado("Veículo removido.", "verde")
+    veiculos = tuple(veiculos_lista_temp)
 
 def loop_inicial():
     opcao = 0
@@ -138,19 +194,18 @@ def loop_inicial():
         elif opcao == 2:
             adicionar_veiculo()
         elif opcao == 3:
-            pass
+            editar_veiculo()
         elif opcao == 4:
-            pass
+            remover_veiculo()
         elif opcao == 5:
             limpa_tela()
             print('Saindo do sistema...')
             aguarda_tecla()
             exit()
         else:
-            print_personalizado('Código incorreto.', 'atenção')
+            print_personalizado('Código incorreto.', 'vermelho')
             aguarda_tecla()
 
 
 if __name__ == '__main__':
-    #loop_inicial()
-    print_personalizado("a", 'b')
+    loop_inicial()
